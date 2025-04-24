3e507b6cad8fde353f4e4fe9a072a841166c55f7
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-04-24 22:16:46 +0700

    Draft phanLoai page layout

--- app/src/main/java/com/example/g_tiu/ui/phanloai/PhanLoaiFragment.java
+++ app/src/main/java/com/example/g_tiu/ui/phanloai/PhanLoaiFragment.java
@@ -24,8 +24,8 @@ public class PhanLoaiFragment extends Fragment {
         binding = FragmentPhanloaiBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textPhanloai;
-        phanLoaiViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textPhanloai;
+//        phanLoaiViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
